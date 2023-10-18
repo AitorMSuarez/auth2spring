508c552061d268567ebe7d188888fe59b0e7f228
aitor.martinez <aitor.martinez@gertek.eus>
2023-10-19 01:04:58 +0200

    Aceptar el compartir los datos de una cuenta de terceros como es la de google.
    ejm spring.
    
    https://docs.spring.io/spring-authorization-server/docs/current/reference/html/guides/how-to-jpa.html#authorization-consent-entity

--- src/main/java/com/ams/authorizationserver/entity/Client.java
+++ src/main/java/com/ams/authorizationserver/entity/Client.java
@@ -51,7 +51,8 @@ public class Client {
 				.clientAuthenticationMethods(am -> am.addAll(client.getAuthenticationMethods()))
 				.authorizationGrantTypes(agt -> agt.addAll(client.getAuthorizationGrantTypes()))
 				.redirectUris(ru -> ru.addAll(client.getRedirectUris())).scopes(sc -> sc.addAll(client.getScopes()))
-				.clientSettings(ClientSettings.builder().requireProofKey(client.isRequireProofKey()).build());
+				.clientSettings(ClientSettings.builder().requireProofKey(client.isRequireProofKey())
+						.requireAuthorizationConsent(true).build());
 		return builder.build();
 	}
 }
